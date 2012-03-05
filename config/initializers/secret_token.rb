e660ac24f712385eef394a245bd0cec7724cbfc8
McFreely <paulrmcfreely@gmail.com>
2012-03-05 15:33:40 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AwesomeList::Application.config.secret_token = '3602a8ae497b78c32c62a1b7cf2045db10c78b26b3a15beb9dd73489df31936aefb081819febf0f8104bcd762141893ecb8d38bb409d1375f75a9129b5849adf'
