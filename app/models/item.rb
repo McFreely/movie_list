22f01036fce6b6421add2479531a9bc9520bb249
McFreely <paulrmcfreely@gmail.com>
2012-03-11 00:23:33 +0100

    validation for title added

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,7 @@
 class Item < ActiveRecord::Base
   
+  validates :title, :presence => true
+  
   belongs_to :user
   scope :completed, where(:completed => true)
   scope :incomplete, where(:completed => false)
