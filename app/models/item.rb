17b3764507f24cbe27790925cb1967734ac88235
McFreely <paulrmcfreely@gmail.com>
2012-03-13 22:55:02 +0100

    resolve problem in development mode and added few layout changes

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,5 @@
 class Item < ActiveRecord::Base
-  attr_accessible :title
-  
   belongs_to :user
-  
-  validates :title, :presence => true, :length => { :maximum => 75 }
   
   scope :completed, where(:completed => true)
   scope :incomplete, where(:completed => false)
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,5 +36,5 @@ AwesomeList::Application.configure do
   config.assets.compress = true
 
   # Expands the lines which load the assets
-  config.assets.debug = true
+  #config.assets.debug = true
 end
